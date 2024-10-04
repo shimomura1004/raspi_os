3528fd9b04e6d9689acc3600bbb38acd0d3fd6af
Sho SHIMOMURA <graueule@gmail.com>
2024-10-04 22:10:48 +0900

    timer

--- src/timer.c
+++ src/timer.c
@@ -3,13 +3,18 @@
 #include "sched.h"
 #include "peripherals/timer.h"
 
+// RPi3 には 1tick ごとにカウントアップするタイマが搭載されていて
+// 合計4個の比較用レジスタがあり、カウンタの値が一致すると対応する割込み線を発火させる
+
 const unsigned int interval = 200000;
 unsigned int curVal = 0;
 
 void timer_init ( void )
 {
+	// 今のカウンタの値を取り出す
 	curVal = get32(TIMER_CLO);
 	curVal += interval;
+	// interval tick 後に発火するように2個目のレジスタに値をセットする
 	put32(TIMER_C1, curVal);
 }
 
@@ -17,6 +22,8 @@ void handle_timer_irq( void )
 {
 	curVal += interval;
 	put32(TIMER_C1, curVal);
+	// TIMER_CS: timer control/status register
+	// todo: "acknowledge" がわからないが、割込みをクリアする？
 	put32(TIMER_CS, TIMER_CS_M1);
 	timer_tick();
 }
