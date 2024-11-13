1624433595a97114aba34360f79f3eb26619d230
Sho SHIMOMURA <graueule@gmail.com>
2024-11-14 01:04:55 +0900

    qemu

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -dtb bcm2710-rpi-3-b.dtb -serial stdio -no-reboot -device usb-kbd -device usb-tablet -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -gdb tcp::12345
+DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -dtb bcm2710-rpi-3-b.dtb -serial stdio -no-reboot -device usb-kbd -device usb-tablet -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -gdb tcp::12345 -S
