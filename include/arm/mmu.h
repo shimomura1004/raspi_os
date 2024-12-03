0a90c56a6055aa035625abd1e5c39add6e7f03bc
Sho SHIMOMURA <graueule@gmail.com>
2024-12-04 00:25:24 +0900

    enable cache

--- include/arm/mmu.h
+++ include/arm/mmu.h
@@ -11,9 +11,11 @@
 #define MM_TYPE_PAGE_TABLE		0x3
 #define MM_TYPE_PAGE 			0x3
 #define MM_TYPE_BLOCK			0x1
+
 #define MM_ACCESS			    (0x1 << 10)
-#define MM_ACCESS_PERMISSION	(0x01 << 6)
 #define MM_nG                   (0x0 << 11)
+#define MM_SH                   (0x3 << 8)
+#define MM_ACCESS_PERMISSION	(0x01 << 6)
 
 // MAIR_EL1 レジスタに設定する値
 // MAIR_EL1 レジスタは8個のパートに分かれており、ページの属性を入れておく
@@ -30,18 +32,27 @@
  *   DEVICE_nGnRnE	000	00000000
  *   NORMAL_NC		001	01000100
  */
-#define MT_DEVICE_nGnRnE 		0x0
-#define MT_NORMAL_NC			0x1
-#define MT_DEVICE_nGnRnE_FLAGS		0x00
-#define MT_NORMAL_NC_FLAGS  		0x44
-#define MAIR_VALUE			(MT_DEVICE_nGnRnE_FLAGS << (8 * MT_DEVICE_nGnRnE)) | (MT_NORMAL_NC_FLAGS << (8 * MT_NORMAL_NC))
+#define MT_DEVICE_nGnRnE            0x0
+#define MT_NORMAL_CACHEABLE         0x1
 
-#define MMU_FLAGS	 		(MM_TYPE_BLOCK | (MT_NORMAL_NC << 2) | MM_nG | MM_ACCESS)
-#define MMU_DEVICE_FLAGS	(MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_nG | MM_ACCESS)
+#define MT_DEVICE_nGnRnE_FLAGS      0x00
+#define MT_NORMAL_CACHEABLE_FLAGS   0xff
+
+#define MAIR_VALUE \
+    (MT_DEVICE_nGnRnE_FLAGS << (8 * MT_DEVICE_nGnRnE)) | \
+    (MT_NORMAL_CACHEABLE_FLAGS << (8 * MT_NORMAL_CACHEABLE))
+
+#define MMU_FLAGS \
+    (MM_TYPE_BLOCK | (MT_NORMAL_CACHEABLE << 2) | MM_nG | MM_ACCESS)
+#define MMU_DEVICE_FLAGS \
+    (MM_TYPE_BLOCK | (MT_DEVICE_nGnRnE << 2) | MM_nG | MM_ACCESS)
 
 #define MM_STAGE2_ACCESS        (1 << 10)
+#define MM_STAGE2_SH            (3 << 8)
 #define MM_STAGE2_MEMATTR       (1 << 0)
-#define MMU_STAGE2_PAGE_FLAGS   (MM_TYPE_PAGE | MM_STAGE2_ACCESS | MM_STAGE2_MEMATTR)
+
+#define MMU_STAGE2_PAGE_FLAGS \
+    (MM_TYPE_PAGE | MM_STAGE2_ACCESS | MM_STAGE2_SH | MM_STAGE2_MEMATTR)
 
 #define TCR_T0SZ			(64 - 48)
 #define TCR_TG0_4K			(0 << 14)
