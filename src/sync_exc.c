9213671c5ddcb8b341c0de144a3041b5603ec4ff
Sho SHIMOMURA <graueule@gmail.com>
2024-12-05 23:33:19 +0900

    split irq.c

---
+++ src/sync_exc.c
@@ -0,0 +1,99 @@
+#include "sync_exc.h"
+#include "arm/sysregs.h"
+#include "sched.h"
+#include "printf.h"
+
+// eclass のインデックスに合わせたエラーメッセージ
+static const char *sync_error_reasons[] = {
+	"Unknown reason.",
+	"Trapped WFI or WFE instruction execution.",
+	"(unknown)",
+	"Trapped MCR or MRC access with (coproc==0b1111).",
+	"Trapped MCRR or MRRC access with (coproc=0b1111).",
+	"Trapped MCR or MRC access with (coproc==0b1110).",
+	"Trapped LDC or STC access.",
+	"Access to SVE, Advanced SIMD, or floating-point functionality trapped by CPACR_EL1.FPEN, CPTR_EL2.FPEN, CPTR_EL2.TFP, or CPTR_EL3.TFP control.",
+	"Trapped VMRS access, from ID group trap.",
+	"Trapped use of a Pointer authentication instruction because HCR_EL2.API == 0 || SCR_EL3.API == 0.",
+	"(unknown)",
+	"(unknown)",
+	"Trapped MRRC access with (coproc==0b1110).",
+	"Branch Target Exception.",
+	"Ilegal Execution state.",
+	"(unknown)",
+	"(unknown)",
+	"SVC instruction execution in AArch32 state.",
+	"HVC instruction execution in AArch32 state.",
+	"SMC instruction execution in AArch32 state.",
+	"(unknown)",
+	"SVC instruction execution in AArch64 state.",
+	"HVC instruction execution in AArch64 state.",
+	"SMC instruction execution in AArch64 state.",
+	"Trapped MSR, MRS or System instruction execution in AArch64 state.",
+	"Access to SVE functionality trapped as a result of CPAR_EL1.ZEN, CPTR_EL2.ZEN, CPTR_EL.TZ, or CPTR_EL3.EZ.",
+	"Trapped ERET, ERETAA, or ERETAB instruction execution.",
+	"(unknown)",
+	"Exception from a Pointer Authentication instruction authentication failure.",
+	"(unknown)",
+	"(unknown)",
+	"(unknown)",
+	"Instruction Abort from a lower Exception level.",
+	"Instruction Abort taken without a change in Exception level.",
+	"PC alignment fault exception.",
+	"(unknown)",
+	"Data Abort from a lower Exception level.",
+	"Data Abort without a change in Exception level, or Data Aborts taken to EL2 as a result of access generated associated with VNCR_EL2 as part of nested virtualization support.",
+	"SP alignment fault exception.",
+	"(unknown)",
+	"Trapped floating-point exception taken from AArch32 state.",
+	"(unknown)",
+	"(unknown)",
+	"(unknown)",
+	"Trapped floating-point exception taken from AArch64 state.",
+	"(unknown)",
+	"(unknown)",
+	"SError interrupt.",
+	"Breakpoint exception from a lower Exception level.",
+	"Breakpoint exception taken without a change in Exception level.",
+	"Software Step exception from a lower Exception level.",
+	"Software Step exception taken without a change in Exception level.",
+	"Watchpoint from a lower Exception level.",
+	"Watchpoint exceptions without a change in Exception level, or Watchpoint exceptions taken to EL2 as a result of accesses generated associated with VNCR_EL2 as part of nested virtualization support.",
+	"(unknown)",
+	"(unknown)",
+	"BKPT instruction execution in AArch32 state.",
+	"(unknown)",
+	"Vector Catch exception from AArch32 state.",
+	"(unknown)",
+	"BRK instruction execution in AArch64 state.",
+};
+
+// todo: 重複
+static const char *entry_error_messages[] = {
+	"SYNC_INVALID_EL2",
+	"IRQ_INVALID_EL2",		
+	"FIQ_INVALID_EL2",		
+	"ERROR_INVALID_EL2",		
+
+	"SYNC_INVALID_EL01_64",		
+	"IRQ_INVALID_EL01_64",		
+	"FIQ_INVALID_EL01_64",		
+	"ERROR_INVALID_EL01_64",		
+
+	"SYNC_INVALID_EL01_32",		
+	"IRQ_INVALID_EL01_32",		
+	"FIQ_INVALID_EL01_32",		
+	"ERROR_INVALID_EL01_32",	
+
+	"SYNC_ERROR",
+	"HVC_ERROR",
+	"DATA_ABORT_ERROR",
+};
+
+// ハンドルできない同期例外が発生した場合の専用のログプリンタ
+void show_uncaught_sync_exception_message(int type, unsigned long esr, unsigned long address) {
+	int eclass = (esr >> ESR_EL2_EC_SHIFT) & 0x3f;
+	printf("%s, ESR: %x, address: %x\nreason: %s\n",
+		entry_error_messages[type], esr,
+		address, sync_error_reasons[eclass]);
+}
