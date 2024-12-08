873e7fca43e6ffc1e7a6f9c1d2dbac158431b3e6
Sho SHIMOMURA <graueule@gmail.com>
2024-12-08 23:36:16 +0900

    add sync exc constants

--- src/sync_exc.c
+++ src/sync_exc.c
@@ -6,10 +6,17 @@
 // ESR_EL2, Exception Syndrome Register (EL2)
 // ***************************************
 
+// ESR_EL2 の中の EC フィールドへのインデックス
 #define ESR_EL2_EC_SHIFT		26
-#define ESR_EL2_EC_TRAP_WFx     1
-#define ESR_EL2_EC_HVC64        22
-#define ESR_EL2_EC_DABT_LOW		36
+
+// EC(exception class)の種類
+// https://developer.arm.com/documentation/ddi0595/2021-03/AArch64-Registers/ESR-EL2--Exception-Syndrome-Register--EL2-?lang=en#fieldset_0-31_26
+#define ESR_EL2_EC_TRAP_WFX     1	// WF* instruction execution
+#define ESR_EL2_EC_TRAP_FP_REG	7	// Access to SVE, Advanced SIMD or floating-point functionality
+#define ESR_EL2_EC_HVC64        22	// HVC instruction execution when HVC is not disabled
+#define ESR_EL2_EC_TRAP_SYSTEM	24	// MSR, MRS or System instruction exection that is not reported using EC 
+#define ESR_EL2_EC_TRAP_SVE		25	// Access to SVE functionality
+#define ESR_EL2_EC_DABT_LOW		36	// Data Abort from a lower Exception level
 
 // eclass のインデックスに合わせたエラーメッセージ
 static const char *sync_error_reasons[] = {
@@ -80,16 +87,26 @@ void handle_sync_exception(unsigned long esr, unsigned long elr, unsigned long f
 	int eclass = (esr >> ESR_EL2_EC_SHIFT) & 0x3f;
 	switch (eclass)
 	{
-	case ESR_EL2_EC_TRAP_WFx:
-		printf("WFx!\n");
+	case ESR_EL2_EC_TRAP_WFX:
+		printf("TRAP_WFX\n");
+		break;
+	case ESR_EL2_EC_TRAP_FP_REG:
+		printf("TRAP_FP_REG\n");
 		break;
 	case ESR_EL2_EC_HVC64:
 		printf("HVC(%d)!\n", hvc_nr);
 		break;
+	case ESR_EL2_EC_TRAP_SYSTEM:
+		printf("TRAP_SYSTEM\n");
+		break;
+	case ESR_EL2_EC_TRAP_SVE:
+		printf("TRAP_SVE\n");
+		break;
 	case ESR_EL2_EC_DABT_LOW:
+		printf("DABT_LOW\n");
 		break;
 	default:
-		printf("Uncaught sync exception: %s, esr: %x, address: %x\n", sync_error_reasons[eclass], esr, elr);
+		printf("Uncaught synchronous exception:\n%s\nesr: %x, address: %x\n", sync_error_reasons[eclass], esr, elr);
 		break;
 	}
 }
