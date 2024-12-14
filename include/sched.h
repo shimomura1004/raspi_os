0e7fccd5fb8fa09c366ff410593898a05b952079
Sho SHIMOMURA <graueule@gmail.com>
2024-12-15 00:36:55 +0900

    add system registers to cpu_sysregs

--- include/sched.h
+++ include/sched.h
@@ -40,12 +40,91 @@ struct cpu_context {
 };
 
 struct cpu_sysregs {
+    // EL0/1 でアクセスしてもトラップされないレジスタ
+    // 切り替え時にレジスタが退避・復帰される
     unsigned long sctlr_el1;
-    unsigned long sps_el1;
     unsigned long ttbr0_el1;
     unsigned long ttbr1_el1;
     unsigned long tcr_el1;
+    unsigned long esr_el1;
+    unsigned long far_el1;
+    unsigned long afsr0_el1;
+    unsigned long afsr1_el1;
     unsigned long mair_el1;
+    unsigned long amair_el1;
+    unsigned long contextidr_el1;
+
+    unsigned long cpacr_el1;
+    unsigned long dczid_el0;
+    unsigned long elr_el1;
+    unsigned long fpcr;
+    unsigned long fpsr;
+    unsigned long isr_el1;
+    unsigned long midr_el1;
+    unsigned long mpidr_el1;
+    unsigned long par_el1;
+    unsigned long sp_el0;
+    unsigned long sp_el1;
+    unsigned long spsr_el1;
+    unsigned long tpidr_el0;
+    unsigned long tpidr_el1;
+    unsigned long tpidrro_el0;
+    unsigned long vbar_el1;
+
+    // HCR_EL2.TACR がセットされている場合にトラップされる
+    unsigned long actlr_el1;        // rw
+
+    // HCR_EL2.TID3 がセットされている場合にトラップされる
+    unsigned long id_pfr0_el1;      // r
+    unsigned long id_pfr1_el1;      // r
+    unsigned long id_mmfr0_el1;     // r
+    unsigned long id_mmfr1_el1;     // r
+    unsigned long id_mmfr2_el1;     // r
+    unsigned long id_mmfr3_el1;     // r
+    unsigned long id_isar0_el1;     // r
+    unsigned long id_isar1_el1;     // r
+    unsigned long id_isar2_el1;     // r
+    unsigned long id_isar3_el1;     // r
+    unsigned long id_isar4_el1;     // r
+    unsigned long id_dsar5_el1;     // r
+    unsigned long mvfr0_el1;        // r
+    unsigned long mvfr1_el1;        // r
+    unsigned long mvfr2_el1;        // r
+    unsigned long id_aa64pfr0_el1;  // r
+    unsigned long id_aa64pfr1_el1;  // r
+    unsigned long id_aa64dfr0_el1;  // r
+    unsigned long id_aa64dfr1_el1;  // r
+    unsigned long id_aa64isar0_el1; // r
+    unsigned long id_aa64isar1_el1; // r
+    unsigned long id_aa64mmfr0_el1; // r
+    unsigned long id_aa64mmfr1_el1; // r
+    unsigned long id_aa64afr0_el1;  // r
+    unsigned long id_aa64afr1_el1;  // r
+
+    // HCR_EL2.TID2 がセットされている場合にトラップされる
+    unsigned long ctr_el0;          // r
+    unsigned long ccsidr_el1;       // r
+    unsigned long clidr_el1;        // r
+    unsigned long csselrel1;        // rw
+
+    // HCR_EL2.TID1 がセットされている場合にトラップされる
+    unsigned long aidr_el1;         // r
+    unsigned long revidr_el1;       // r
+
+    // system timer
+    unsigned long cntfrq_el0;
+    unsigned long cntkctl_el1;
+    unsigned long cntp_ctl_el0;
+    unsigned long cntp_cval_el0;
+    unsigned long cntp_tval_el0;
+    unsigned long cntpct_el0;
+    unsigned long cntps_ctl_el1;
+    unsigned long cntps_cval_el1;
+    unsigned long cntps_tval_el1;
+    unsigned long cntv_ctl_el0;
+    unsigned long cntv_cval_el0;
+    unsigned long cntv_tval_el0;
+    unsigned long cntvct_el0;
 };
 
 struct mm_struct {
