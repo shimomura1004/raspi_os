35f3fb420c6cfc3f5fbea0b24944cf50b1026d7a
Sho SHIMOMURA <graueule@gmail.com>
2025-01-01 11:34:48 +0900

    add fat32 structures

--- src/fat.c
+++ src/fat.c
@@ -23,10 +23,13 @@
  *
  */
 
+#include <inttypes.h>
 #include "sd.h"
 #include "debug.h"
 #include "mm.h"
 
+// BPB: 各パーティションの最初のセクタに存在する領域で
+//      ファイルシステムの物理レイアウトやパラメータを記録するもの
 // セクタ: ディスク上の最小のデータ単位(通常は512バイト)
 // クラスタ: 複数のセクタをまとめたもので、ファイルを格納する単位
 //          (4KB(8セクタ) や 32KB(64セクタ) などが多い)
@@ -53,46 +56,126 @@ int memcmp(void *s1, void *s2, int n)
 // 最初の FAT パーティションのアドレス
 static unsigned int partitionlba = 0;
 
-// BPB は各パーティションの最初のセクタに存在する領域で
-// ファイルシステムの物理レイアウトやパラメータを記録するもの
-// the BIOS Parameter Block (in Volume Boot Record)
-typedef struct {
-    char            jmp[3];     // BS_JmpBoot
-    char            oem[8];     // BS_OEMName
-    unsigned char   bps0;       // BPB_BytsPerSec
-    unsigned char   bps1;       // BPB_BytsPerSec
-    unsigned char   spc;        // BPB_SecPerClus
-    unsigned short  rsc;        // BPB_RsvdSecCnt
-    unsigned char   nf;         // BPB_NumFATs
-    unsigned char   nr0;        // BPB_RootEntCnt
-    unsigned char   nr1;        // BPB_RootEntCnt
-    unsigned short  ts16;       // BPB_TotSec16
-    unsigned char   media;      // BPB_Media
-    unsigned short  spf16;      // BPB_FATSz16
-    unsigned short  spt;        // BPB_SecPerTrk
-    unsigned short  nh;         // BPB_NumHeads
-    unsigned int    hs;         // BPB_HiddSec
-    unsigned int    ts32;       // BPB_TotSec32
-    unsigned int    spf32;      // BPB_FATSz32
-    unsigned int    flg;        // BPB_ExtFlags, BPB_FSVer
-    unsigned int    rc;         // BPB_RootClus
-    // todo: この下のあたりのオフセットが合わない？
-    char            vol[6];     // BPB_FSInfo...
-    char            fst[8];     //
-    char            dmy[20];    // ...BS_VolLab
-    char            fst2[8];    // BS_FilSysType
-} __attribute__((packed)) bpb_t;
+#define FAT32_BOOT              0
+#define FAT32_MAX_FILENAME_LEN  255
+#define BLOCKSIZE               512
 
-// directory entry structure
-typedef struct {
-    char            name[8];    // DIR_Name
-    char            ext[3];     // DIR_Name(拡張子)
-    char            attr[9];    // DIR_Attr, DIR_NTRes, ...
-    unsigned short  ch;         // DIR_FstClusHI
-    unsigned int    attr2;      // DIR_WrtTime, DIR_WrtDate
-    unsigned short  cl;         // DIR_FstClusLO
-    unsigned int    size;       // DIR_FileSize
-} __attribute__((packed)) fatdir_t;
+// the BIOS Parameter Block (in Volume Boot Record)
+typedef struct fat32_boot {
+    uint8_t     BS_JmpBoot[3];
+    uint8_t     BS_OEMName[8];
+    uint16_t    BPB_BytsPerSec;
+    uint8_t     BPB_SecPerClus;
+    uint16_t    BPB_RsvdSecCnt;
+    uint8_t     BPB_NumFATs;
+    uint16_t    BPB_RootEntCnt;
+    uint16_t    BPB_TotSec16;
+    uint8_t     BPB_Media;
+    uint16_t    BPB_FATSz16;
+    uint16_t    BPB_SecPerTrk;
+    uint16_t    BPB_NumHeads;
+    uint32_t    BPB_HiddSec;
+    uint32_t    BPB_TotSec32;
+    uint32_t    BPB_FATSz32;
+    uint32_t    BPB_ExtFlags;
+    uint32_t    BPB_FSVer;
+    uint32_t    BPB_RootClus;
+    uint16_t    BPB_FSInfo;
+    uint16_t    BPB_BkBootSec;
+    uint8_t     BPB_Reserved[12];
+    uint8_t     BS_DrvNum;
+    uint8_t     BS_Reserved1;
+    uint8_t     BS_BootSig;
+    uint32_t    BS_VolID;
+    uint8_t     BS_VolLab[11];
+    uint8_t     BS_FilSysType[8];
+    uint8_t     BS_BootCode32[420];
+    uint16_t    BS_BootSign;
+} __attribute__((__packed__));
+
+// file system info
+struct fat32_fsi {
+    uint32_t    FSI_LeadSig;
+    uint8_t     FSI_Reserved1[480];
+    uint32_t    FSI_StrucSig;
+    uint32_t    FSI_Free_Count;
+    uint32_t    FSI_Nxt_Free;
+    uint8_t     FSI_Reserved2[12];
+    uint32_t    FSI_TrailSig;
+} __attribute__((__packed__));
+
+#define ATTR_READ_ONLY   0x01
+#define ATTR_HIDDEN      0x02
+#define ATTR_SYSTEM      0x04
+#define ATTR_VOLUME_ID   0x08
+#define ATTR_DIRECTORY   0x10
+#define ATTR_ARCHIVE     0x20
+#define ATTR_LONG_NAME   (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
+
+// directory entry
+struct fat32_dent {
+    uint8_t     DIR_Name[11];
+    uint8_t     DIR_Attr;
+    uint8_t     DIR_NTRes;
+    uint8_t     DIR_CrtTimeTenth;
+    uint16_t    DIR_CrtTime;
+    uint16_t    DIR_CrtDate;
+    uint16_t    DIR_LstAccDate;
+    uint16_t    DIR_FstClusHI;
+    uint16_t    DIR_WrtTime;
+    uint16_t    DIR_WrtDate;
+    uint16_t    DIR_FstClusLO;
+    uint32_t    DIR_FileSize;
+} __attribute__((__packed___));
+
+// long file name entry
+struct fat32_lfnent {
+    uint8_t     LDIR_Ord;
+    uint8_t     LDIR_Name1[10];
+    uint8_t     LDIR_Attr;
+    uint8_t     LDIR_Type;
+    uint8_t     LDIR_Chksum;
+    uint8_t     LDIR_Name2[12];
+    uint16_t    LDIR_FstClusLO;
+    uint8_t     LDIR_Name3[4];
+} __attribute__((__packed__));
+
+enum fat32_file_type {
+    FAT32_REGULAR,
+    FAT32_DIR,
+};
+
+struct fat32_file {
+    struct fat32_fs *fat32;
+    uint8_t attr;
+    uint32_t size;
+    uint32_t cluster;
+};
+
+struct fat32_fs {
+    struct fat32_boot boot;
+    struct fat32_fsi fsi;
+    uint32_t fatstart;
+    uint32_t fatsectors;
+    uint32_t rootstart;
+    uint32_t rootsectors;
+    uint32_t datastart;
+    uint32_t datasectors;
+    struct fat32_file root;
+};
+
+// クラスタ番号
+// 0                         : 未使用クラスタ
+// 1                         : Reserved
+// 2           - 0x0fff_fff6 : 有効なクラスタ
+// 0x0fff_fff7               : 不良クラスタ
+// 0x0fff_fff8 - 0x0fff_ffff : クラスタ終端
+#define is_active_cluster(c) (0x2 <= (c) && (c) < 0x0ffffff6)
+#define is_terminal_cluster(c) (0x0ffffff8 <= (c) && (c) <= 0x0fffffff)
+#define dir_cluster(dir) (((dir)->DIR_FstClusHI << 16) | (dir)->DIR_FstClusLO)
+#define UNUSED_CLUSTER      0
+#define RESERVED_CLUSTER    1
+#define BAD_CLUSTER         0x0ffffff7
 
 /**
  * Get the starting LBA address of the first partition
