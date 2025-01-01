aba1cc76966d33cd1b75029ff44d0114f21cc4c8
Sho SHIMOMURA <graueule@gmail.com>
2025-01-01 23:44:55 +0900

    replace fat related code

---
+++ include/fat32.h
@@ -0,0 +1,15 @@
+#ifndef _FAT32_H
+#define _FAT32_H
+
+#include <stddef.h>
+
+struct fat32_fs;
+struct fat32_file;
+
+int fat32_get_handle(struct fat32_fs *);
+int fat32_lookup(struct fat32_fs *, const char *, struct fat32_file *);
+int fat32_read(struct fat32_file *, void *, unsigned long, size_t);
+int fat32_file_size(struct fat32_file *);
+int fat32_is_directory(struct fat32_file *);
+
+#endif
