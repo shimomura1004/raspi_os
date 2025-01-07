3de7592ddd9c0a648164468d697d13519c23417c
Sho SHIMOMURA <graueule@gmail.com>
2025-01-08 00:10:30 +0900

    remove warning

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@
 #DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s -S
 #DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
 
-DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -sd ./fs.img -s $*
+DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -drive format=raw,file=./fs.img -s $*
