3db88bdf28cc2200efdbfbed4d1cf3c7125bad61
SHIMOMURA Sho <graueule@gmail.com>
2025-01-16 17:43:46 +0900

    format

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -1,77 +1,78 @@
-#include "utils.h"
 #include "peripherals/mini_uart.h"
 #include "peripherals/gpio.h"
+#include "printf.h"
+#include "utils.h"
 
-static void _uart_send ( char c )
-{
-	// 送信バッファが空くまで待つビジーループ
-	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x20) 
-			break;
-	}
-	put32(AUX_MU_IO_REG,c);
+static void _uart_send(char c) {
+    // 送信バッファが空くまで待つビジーループ
+    while (1) {
+        if (get32(AUX_MU_LSR_REG) & 0x20) {
+            break;
+        }
+    }
+    put32(AUX_MU_IO_REG, c);
 }
 
 void uart_send(char c) {
-	if (c == '\n') {
-		_uart_send('\r');
-		_uart_send('\n');
-	}
-	else {
-		_uart_send(c);
-	}
+    if (c == '\n') {
+        _uart_send('\r');
+        _uart_send('\n');
+    } else {
+        _uart_send(c);
+    }
 }
 
-char uart_recv ( void )
-{
-	// 受信バッファにデータが届くまで待つビジーループ
-	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x01) 
-			break;
+char uart_recv(void) {
+    // 受信バッファにデータが届くまで待つビジーループ
+    while (1) {
+        if (get32(AUX_MU_LSR_REG) & 0x01) {
+            break;
+        }
+    }
+
+	char c = get32(AUX_MU_IO_REG) & 0xFF;
+	if (c == '\r') {
+		return '\n';
 	}
-	return(get32(AUX_MU_IO_REG)&0xFF);
+    return c;
 }
 
-void uart_send_string(char* str)
-{
-	for (int i = 0; str[i] != '\0'; i ++) {
-		_uart_send((char)str[i]);
-	}
+void uart_send_string(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        _uart_send((char)str[i]);
+    }
 }
 
-void uart_init ( void )
-{
-	unsigned int selector;
+void uart_init(void) {
+    unsigned int selector;
 
-	selector = get32(GPFSEL1);
-	selector &= ~(7<<12);                   // clean gpio14
-	// 2(0b010) は alt5 を選ぶという意味 
-	selector |= 2<<12;                      // set alt5 for gpio14
-	selector &= ~(7<<15);                   // clean gpio15
-	selector |= 2<<15;                      // set alt5 for gpio15
-	put32(GPFSEL1,selector);
+    selector = get32(GPFSEL1);
+    selector &= ~(7 << 12); // clean gpio14
+    // 2(0b010) は alt5 を選ぶという意味
+    selector |= 2 << 12;    // set alt5 for gpio14
+    selector &= ~(7 << 15); // clean gpio15
+    selector |= 2 << 15;    // set alt5 for gpio15
+    put32(GPFSEL1, selector);
 
-	// GPIO のpull-up/pull-down の設定をクリアする
-	put32(GPPUD,0);
-	delay(150);
-	put32(GPPUDCLK0,(1<<14)|(1<<15));
-	delay(150);
-	put32(GPPUDCLK0,0);
+    // GPIO のpull-up/pull-down の設定をクリアする
+    put32(GPPUD, 0);
+    delay(150);
+    put32(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+    put32(GPPUDCLK0, 0);
 
-	// UART としての設定を行う
-	put32(AUX_ENABLES,1);                   //Enable mini uart (this also enables access to it registers)
-	put32(AUX_MU_CNTL_REG,0);               //Disable auto flow control and disable receiver and transmitter (for now)
-	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
-	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
-	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG,270);             //Set baud rate to 115200
+    // UART としての設定を行う
+    put32(AUX_ENABLES,
+          1); // Enable mini uart (this also enables access to it registers)
+    put32(AUX_MU_CNTL_REG, 0); // Disable auto flow control and disable receiver
+                               // and transmitter (for now)
+    put32(AUX_MU_IER_REG, 0);    // Disable receive and transmit interrupts
+    put32(AUX_MU_LCR_REG, 3);    // Enable 8 bit mode
+    put32(AUX_MU_MCR_REG, 0);    // Set RTS line to be always high
+    put32(AUX_MU_BAUD_REG, 270); // Set baud rate to 115200
 
-	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
+    put32(AUX_MU_CNTL_REG, 3); // Finally, enable transmitter and receiver
 }
-
 
 // This function is required by printf function
-void putc ( void* p, char c)
-{
-	_uart_send(c);
-}
+void putc(void *p, char c) { _uart_send(c); }
