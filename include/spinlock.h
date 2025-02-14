bd651ae230b01941be9d4992f323bfd751e23853
SHIMOMURA Sho <graueule@gmail.com>
2025-02-15 00:53:57 +0900

    add spinlock

---
+++ include/spinlock.h
@@ -0,0 +1,15 @@
+#ifndef _SPINLOCK_H
+#define _SPINLOCK_H
+
+struct spinlock {
+    unsigned long locked;
+    char *name;
+    // todo: もう少し情報を追加して構造体にする
+    long cpuid;
+};
+
+void init_lock(struct spinlock *lock, char *name);
+void acquire_lock(struct spinlock *lock);
+void release_lock(struct spinlock *lock);
+
+#endif
---
+++ src/spinlock.c
@@ -0,0 +1,42 @@
+#include "spinlock.h"
+#include "sched.h"
+#include "utils.h"
+#include "debug.h"
+#include "spinlock.h"
+#include "irq.h"
+
+extern void spinlock_acquire(struct spinlock *);
+extern void spinlock_release(struct spinlock *);
+
+static int holding(struct spinlock *lock) {
+    return lock->locked && lock->cpuid == get_cpuid();
+}
+
+void init_lock(struct spinlock *lock, char *name) {
+    lock->locked = 0;
+    lock->name = name;
+    lock->cpuid = -1;
+}
+
+void acquire_lock(struct spinlock *lock) {
+    // todo: ロック中は割込みを禁止しておかないとデッドロックする可能性がある
+// disable_irq();
+
+    if (holding(lock)) {
+        PANIC("acquire: already locked by myself");
+    }
+
+    spinlock_acquire(&lock->locked);
+    lock->cpuid = get_cpuid();
+}
+
+void release_lock(struct spinlock *lock) {
+    if (!holding(lock)) {
+        PANIC("release: not locked");
+    }
+
+    lock->cpuid = -1;
+    spinlock_release(&lock->locked);
+
+// enable_irq();
+}
