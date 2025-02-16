3b88c378f95c051b36ac4300580d3bf4266932f5
Sho SHIMOMURA <graueule@gmail.com>
2025-02-16 10:52:56 +0900

    add get_cpuid/get_sp

--- include/utils.h
+++ include/utils.h
@@ -20,6 +20,8 @@ extern unsigned long get_ttbr0_el1();
 extern unsigned long get_ttbr1_el1();
 extern unsigned long get_ttbr0_el2();
 extern unsigned long get_vttbr_el2();
+extern unsigned long get_cpuid();
+extern unsigned long get_sp();
 
 // Stage2 変換テーブルをセットしてアドレス空間(VTTBR_EL2)を切り替え、つまり IPA -> PA の変換テーブルを切り替える
 //   テーブル自体の準備は VM がロードされた初期化時やメモリアボート時に行う
