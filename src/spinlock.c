c5f7010672f8054c55becb942e85eacbeccbec3b
Sho SHIMOMURA <graueule@gmail.com>
2025-02-18 01:47:17 +0900

    add cpu struct

--- src/spinlock.c
+++ src/spinlock.c
@@ -5,6 +5,28 @@
 #include "spinlock.h"
 #include "irq.h"
 
+// todo: 正しい位置に
+#define NUMBER_OF_CPUS 4
+struct cpu_struct cpus[NUMBER_OF_CPUS];
+void init_cpus() {
+	for (int i = 0; i < NUMBER_OF_CPUS; i++) {
+		cpus[i].current = NULL;
+		cpus[i].number_of_off = 0;
+		cpus[i].interrupt_enable = 0;
+	}
+}
+
+// todo: current_cpu から戻るときに戻りアドレス x30 の値がおかしい
+//       アセンブラとのあいだで変数のサイズがあってない？
+//       スタックが他コアと共有されていて破壊されている？ -> fixed
+//       割込みによるレジスタ保全が不十分？
+struct cpu_struct *current_cpu() {
+    unsigned long cpuid = get_cpuid();
+    return cpus + cpuid;
+//	return &cpus[get_cpuid()];
+}
+
+
 extern void _spinlock_acquire(struct spinlock *);
 extern void _spinlock_release(struct spinlock *);
 
