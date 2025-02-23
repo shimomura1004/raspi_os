b85eb8940833201b95d8a62d831276a81af21417
SHIMOMURA Sho <graueule@gmail.com>
2025-02-23 10:37:22 +0900

    disable interrupt while spinlock

--- src/spinlock.c
+++ src/spinlock.c
@@ -37,7 +37,7 @@ void init_lock(struct spinlock *lock, char *name) {
 
 void acquire_lock(struct spinlock *lock) {
     // todo: ロック中は割込みを禁止しておかないとデッドロックする可能性がある
-// disable_irq();
+push_disable_irq();
 
     if (holding(lock)) {
         PANIC("acquire: already locked by myself");
@@ -55,5 +55,36 @@ void release_lock(struct spinlock *lock) {
     lock->cpuid = -1;
     _spinlock_release(&lock->locked);
 
-// enable_irq();
+pop_disable_irq();
+}
+
+// 多重で CPU 割込みを禁止するときに使う、割込み禁止関数
+// push された数と同じだけ pop しないと割込みが有効にならない
+void push_disable_irq() {
+    // todo: interrupt_enable は必要ないのでは
+    // int old = is_interrupt_enabled();
+    disable_irq();
+
+    struct cpu_struct *cpu = current_cpu();
+    if (cpu->number_of_off == 0) {
+        // cpu->interrupt_enable = old;
+    }
+    cpu->number_of_off++;
+}
+
+void pop_disable_irq() {
+    struct cpu_struct *cpu = current_cpu();
+    if (is_interrupt_enabled()) {
+        PANIC("interruptible");
+    }
+    if (cpu->number_of_off <= 0) {
+        PANIC("number_of_off is 0");
+    }
+
+    cpu->number_of_off--;
+    if (cpu->number_of_off == 0) {
+        // todo: ここでクラッシュしている？
+        // 片方のコアが文字列を出力中なのに、もう片方のコアが enable しようとしてる
+        enable_irq();
+    }
 }
