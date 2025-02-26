a12cc21447d04acdd05e7e7444e8d3555e909d73
SHIMOMURA Sho <graueule@gmail.com>
2025-02-26 23:14:14 +0900

    remove note

--- include/cpu_core.h
+++ include/cpu_core.h
@@ -10,7 +10,6 @@ struct cpu_core_struct {
     // 排他制御時に割込みを禁止するとき、何回割込み禁止が要求されたかを保持する
     // 全員が割込み禁止を解除したら、本当に割込みを許可する
     int number_of_off;
-    // todo: 使ってない、削除する？
     int interrupt_enable;
 };
 
