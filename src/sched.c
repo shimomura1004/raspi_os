010215fd61b8140ce52559900fd6e5dcec4b2f25
SHIMOMURA Sho <graueule@gmail.com>
2025-03-18 01:37:48 +0900

    add note

--- src/sched.c
+++ src/sched.c
@@ -126,6 +126,7 @@ static void _schedule(void)
 	switch_to(vms[next]);
 
 	// todo: xv6 と同じように、タスク間で直接切り替えるのではなく、一度 idle_vm を経由するようにする
+	//       各 vm から cpu ごとの idle_vm に戻るための関数 yield を用意する
 }
 
 // 自主的に CPU 時間を手放し VM を切り替える
