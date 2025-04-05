f6a0af5fea3ef8886ea18e827744a257460f4f21
SHIMOMURA Sho <graueule@gmail.com>
2025-04-05 17:24:18 +0900

    rename

--- example/test_binary/src/main.c
+++ example/test_binary/src/main.c
@@ -2,13 +2,13 @@
 
 char *hello_str = "hello world";
 
-struct raw_binary_loader_args echo_bin_args = {
+struct loader_args echo_bin_args = {
 	.loader_addr = 0x0,
 	.entry_point = 0x0,
 	.sp = 0x100000,
 	.filename = "ECHO.ELF",
 };
-struct raw_binary_loader_args *echo_bin_args_p = &echo_bin_args;
+struct loader_args *echo_bin_args_p = &echo_bin_args;
 
 int main() {
   return 0;
