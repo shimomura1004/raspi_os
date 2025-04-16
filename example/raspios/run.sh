df77bcecc13445c3f131bb8b7768f4f10e6bc161
Sho SHIMOMURA <graueule@gmail.com>
2025-04-16 23:05:46 +0900

    raspios runs without hypervisor

--- example/raspios/run.sh
+++ example/raspios/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -nographic -serial null -serial mon:stdio -s $*
+
+# -kernel オプションを使うと el2 から開始できるが、
+# イメージをロードするアドレスを指定できないのでフラットバイナリが使えない
+#DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -kernel kernel8.img -nographic -serial null -serial mon:stdio -s $*
+
+DISPLAY=:0 qemu-system-aarch64 -m 1024 -M raspi3b -device loader,file=./kernel8.img,addr=0x0 -nographic -serial null -serial mon:stdio -s $*
