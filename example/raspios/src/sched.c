cbf28191b92005030ab66799f5d1edc6c39f8a1e
Sho SHIMOMURA <graueule@gmail.com>
2025-04-21 00:07:02 +0900

    retry if context switching failed

--- example/raspios/src/sched.c
+++ example/raspios/src/sched.c
@@ -29,32 +29,38 @@ void _schedule(void)
 	int next,c;
 	struct task_struct * p;
 
-	// ここではデータを操作しない、切り替え先を探すだけ
 	while (1) {
-		c = -1;
-		next = 0;
-		for (int i = 0; i < NR_TASKS; i++){
-			p = task[i];
-			if (p && p->state != TASK_ZOMBIE && p->counter > c) {
-				c = p->counter;
-				next = i;
+		// ここではデータを操作しない、切り替え先を探すだけ
+		while (1) {
+			c = -1;
+			next = 0;
+			for (int i = 0; i < NR_TASKS; i++){
+				p = task[i];
+				if (p && p->state != TASK_ZOMBIE && p->counter > c) {
+					c = p->counter;
+					next = i;
+				}
+			}
+			if (c) {
+				break;
+			}
+			for (int i = 0; i < NR_TASKS; i++) {
+				p = task[i];
+				if (p) {
+					p->counter = (p->counter >> 1) + p->priority;
+				}
 			}
 		}
-		if (c) {
+
+		// 実際に切り替える
+		// switch_to を呼ぶと、切り替えに成功した場合は呼び出したプロセスのコンテキストには戻ってこず
+		// 切り替え先のプロセスのコンテキストでここに戻ってくる
+		int switched = switch_to(task[next]);
+
+		if (switched) {
 			break;
 		}
-		for (int i = 0; i < NR_TASKS; i++) {
-			p = task[i];
-			if (p) {
-				p->counter = (p->counter >> 1) + p->priority;
-			}
-		}
 	}
-
-	// 実際に切り替える
-	// switch_to を呼ぶと、切り替えに成功した場合は呼び出したプロセスのコンテキストには戻ってこず
-	// 切り替え先のプロセスのコンテキストでここに戻ってくる
-	switch_to(task[next]);
 
 	preempt_enable();
 }
@@ -65,33 +71,39 @@ void schedule(void)
 	_schedule();
 }
 
-// コンテキスト切り替えが不要だった場合は -1
-// 実際にコンテキストが切り替わった場合は 0
+// コンテキスト切り替えが不要だった場合は 0
+// 実際にコンテキストが切り替わった場合は 1
 // switch_to を呼び出したプロセスにすぐ値が戻るわけではなく、
 // しばらくしてまたこのプロセスに CPU 時間が回ってきたあとに返される値であることに注意
 // 切り替わった先のプロセスがこの関数が返す値は、自分が前回 switch_to を呼び出したときの結果
 int switch_to(struct task_struct * next) 
 {
 printf("switch from 0x%x to 0x%x\n", current, next);
+
 	// 切り替えが不要だった場合はなにもせず -1 を返す
 	if (current == next || next->state == TASK_RUNNING) {
 printf("no switch\n");
-		return -1;
+		return 0;
 	}
 
 	struct task_struct * prev = current;
 	current = next;
 	set_pgd(next->mm.pgd);
 
+	// この prev は先ほどまでの current
+	// このプロセスのコンテキストにおいて、これまで実行していたプロセス自体を指す
 	prev->state = TASK_RUNNABLE;
 	cpu_switch_to(prev, next);
+	// cpu_switch_to したあとここに戻ってきた場合、
+	// この prev は少し前に sched を呼んで休止した自分自身のプロセスを指している
 	prev->state = TASK_RUNNING;
+
 printf("now 0x%x\n", current);
 
 	// cpu_switch_to で別のプロセスに切り替わったあと、
 	// しばらくしてまたこのプロセスのコンテキストに戻ってきた場合は、
-	// 切り替えが成功していたとして 0 を返す
-	return 0;
+	// 切り替えが成功していたとして 1 を返す
+	return 1;
 }
 
 void schedule_tail(void) {
